417108cf31758dc5f473882e4f4c16da6237a28d
Larissa Silva <larissa.gcrs@gmail.com>
2020-09-07 01:04:16 -0300

    feat: add routes empresas and usuarios

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,20 @@
 const express = require('express');
 
+const UsuarioController = require('./controllers/UsuarioController');
+const EmpresaController = require('./controllers/EmpresaController');
+
 const routes = express.Router();
 
 routes.get('/', (req, res) => {
     res.send('LevelUp, Girl! Backend');
-})
+});
+
+routes.get('/usuarios', UsuarioController.index);
+routes.get('/usuario/:id', UsuarioController.show);
+routes.post('/usuario/cadastrar', UsuarioController.store);
+
+routes.get('/empresas', EmpresaController.index);
+routes.get('/empresa/:id', EmpresaController.show);
+routes.post('/empresa/cadastrar', EmpresaController.store);
 
 module.exports = routes;
